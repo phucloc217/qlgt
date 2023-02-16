6bd3dd0bcff574f192d3f0ddfeb25ccafd27bc8c
phucloc2107 <loc.nguyen456789@gmail.com>
2023-02-16 10:35:41 +0700

    Trang dang nhap

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,21 @@ import './bootstrap';
 import {createApp} from 'vue'
 import {createRouter, createWebHistory} from 'vue-router';
 import App from './App.vue'
+import './js/config'
+import './bootstrap5';
+import './helpers';
+import './menu';
+import './js/main';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
             path: '/home',
             component: () => import('./pages/index.vue')
+        },{
+            path: '/login',
+            component: () => import('./pages/login.vue')
         },
        
     ],
