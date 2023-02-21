12bbbdcc9cd02e2acba4f198f2a9853a64b46c44
Phucloc217 <loc.nguyen456789@gmail.com>
2023-02-21 22:20:56 +0700

    Chuc nang tai ma QR

--- resources/js/pages/QRCode.vue
+++ resources/js/pages/QRCode.vue
@@ -2,14 +2,16 @@
   <div class="container d-flex align-items-center justify-content-center p-5">
     <div class="p-3 mb-2 bg-white m-auto w-auto shadow mb-5 bg-body rounded">
       <div>
-        <qrcode-vue :value="value" :size="size" level="H" class="m-auto"/>
+        <qrcode-vue :value="value" :size="size" level="H" class="m-auto" id="qrcode" />
       </div>
       <div class="row mt-3 m-auto w-100">
-        <CButton type="submit" color="primary"><CIcon icon="cilDataTransferDown" size="md"/> Tải về</CButton>
+        <CButton type="submit" color="primary"  @click="download" >
+          <CIcon icon="cilDataTransferDown" size="md"/> Tải về
+        </CButton>
       </div>
-     
+
     </div>
-   
+
   </div>
 </template>
   
@@ -20,6 +22,16 @@ export default {
     return {
       value: '12354',
       size: 300,
+    }
+  },
+  methods: {
+    download(event) {
+      var canvas = document.getElementById("qrcode");
+      let image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+      var link = document.createElement('a');
+      link.download = "qrcode.png";
+      link.href = image;
+      link.click();
     }
   },
   components: {
